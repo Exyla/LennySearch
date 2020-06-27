1e055e6afb743437c69d1f18ae574e4bc762a10a
AyzansAlt <daberdabs24@gmail.com>
2020-06-27 12:06:19 +0000

    v1.0.0

--- script.js
+++ script.js
@@ -5,10 +5,13 @@ const lennyFaces = [
     "^‿^",
     "(づ◔ ͜ʖ◔)づ",
     "(⌐■_■)",
-    "ಠ_ಠ"
+    "ಠ_ಠ",
+    "" //<---- This is needed
     //These are where the faces are stored
 ]
 
 var number =  Math.floor((Math.random() * lennyFaces.length) + 1);
 var face = document.getElementById("LennyText");
 face.textContent = lennyFaces[number]
+
+console.log(number)
